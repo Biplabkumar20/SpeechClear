1a61e9e4640d93fdbed2036b6349bef384e464ae
Biplab kumar <43509655+Biplabkumar20@users.noreply.github.com>
2021-12-16 13:43:52 +0530

    Add files via upload

---
+++ speechclr.js
@@ -0,0 +1,40 @@
+const  SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+let clicker = document.querySelector('#speechInput');
+const outputResult = document.querySelector('.output');
+let startFlag = false;
+
+clicker.addEventListener('click', e => {
+    // console.log('clicked');
+    if(!startFlag) {
+        recognition.start();
+        startFlag = true;
+        outputResult.textContent = 'Listening...';
+    }
+    
+});
+
+recognition.addEventListener('result', e => {
+    console.log(e.results[0][0].transcript);
+    const color = e.results[0][0].transcript.replace(' ','');
+    outputResult.textContent = color;
+    //clicker.style.backgroundColor = color;
+    startFlag = false;
+    document.body.style.backgroundColor = color;
+});
+
+recognition.addEventListener('end', e => {
+    recognition.stop();
+    startFlag = false;
+});
+
+recognition.addEventListener('error', e => {
+    outputResult.textContent = ' Error occured in recognition';
+    startFlag = false;
+});
+
+recognition.addEventListener('nomatch', e => {
+    outputResult.textContent = ' Input does not match';
+    startFlag = false;
+});
